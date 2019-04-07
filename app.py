dd18ed4d7c7e4dcfe4886fc6676992f3add3db6b
Mery Jimenez <kantaranas@gmail.com>
2019-04-07 00:37:05 -0400

    updates

--- app.py
+++ app.py
@@ -1,187 +1,8 @@
-# 1. import Flask
-# from flask import Flask
-# from flask import jsonify
-
-# # 2. Create an app, being sure to pass __name__
-# app = Flask(__name__)
-
-
-# # 3. Define what to do when a user hits the index route
-# # route will tell the server based on the clients choice 
-# # what to return, it is basically the menu
-# # Flask Routes
-# @app.route("/")
-# def home():
-#     print("My Trip Precipitacion")
-#     return "I was lucky, I encountered little rain"
-
-
-# # 4. Define what to do when a user hits the /about route
-# @app.route("/api/v1.0/precipitation")
-# def precipitation():
-#     return (
-#         f"Welcome to the Justice League API!<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/justice-league<br/>"
-#         f"/api/v1.0/justice-league/superhero/batman"
-#     )
-
-# # 5. Define what to do when a user hits the /about route
-# @app.route("/contact")
-# def contact():
-#     print("Server received request for 'Contact' page...")
-#     return "Please email me at mery@mypage.com"
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-# #-------------------------------------------
-
-# hello_dict = {"Hello": "World!"}
-# hello_str = "Hello World"
-
-# @app.route("/")
-# def home():
-#     return "Hi"
-
-# @app.route("/normal")
-# def normal():
-#     return hello_str
-
-# @app.route("/jsonified")
-# def jsonified():
-#     return jsonify(hello_dict)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)    
-
-# #------------------------------------
-
-# from flask import Flask, jsonify
-
-# justice_league_members = [
-#     {"superhero": "Aquaman", "real_name": "Arthur Curry"},
-#     {"superhero": "Batman", "real_name": "Bruce Wayne"},
-#     {"superhero": "Cyborg", "real_name": "Victor Stone"},
-#     {"superhero": "Flash", "real_name": "Barry Allen"},
-#     {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
-#     {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
-#     {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
-# ]
-
-# # Flask Setup
-# app = Flask(__name__)
-
-
-# # Flask Routes
-# @app.route("/api/v1.0/precipitation")
-# def justice_league():
-#     """Return the justice league data as json"""
-
-#     return jsonify(justice_league_members)
-
-
-# @app.route("/")
-# def welcome():
-#     return (
-#         f"Welcome to the Justice League API!<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/justice-league<br/>"
-#         f"/api/v1.0/justice-league/Arthur%20Curry<br/>"
-#         f"/api/v1.0/justice-league/Bruce%20Wayne<br/>"
-#         f"/api/v1.0/justice-league/Victor%20Stone<br/>"
-#         f"/api/v1.0/justice-league/Barry%20Allen<br/>"
-#         f"/api/v1.0/justice-league/Hal%20Jordan<br/>"
-#         f"/api/v1.0/justice-league/Clark%20Kent/Kal-El<br/>"
-#         f"/api/v1.0/justice-league/Princess%20Diana"
-#     )
-
-
-# @app.route("/api/v1.0/justice-league/<real_name>")
-# def justice_league_character(real_name):
-#     """Fetch the Justice League character whose real_name matches
-#        the path variable supplied by the user, or a 404 if not."""
-
-#     canonicalized = real_name.replace(" ", "").lower()
-#     for character in justice_league_members:
-#         search_term = character["real_name"].replace(" ", "").lower()
-
-#         if search_term == canonicalized:
-#             return jsonify(character)
-
-#     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-# # -----------------------------------------------
-
-# from flask import Flask, jsonify
-
-# justice_league_members = [
-#     {"superhero": "Aquaman", "real_name": "Arthur Curry"},
-#     {"superhero": "Batman", "real_name": "Bruce Wayne"},
-#     {"superhero": "Cyborg", "real_name": "Victor Stone"},
-#     {"superhero": "Flash", "real_name": "Barry Allen"},
-#     {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
-#     {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
-#     {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
-# ]
-
-# #################################################
-# # Flask Setup
-# #################################################
-# app = Flask(__name__)
-
-
-# #################################################
-# # Flask Routes
-# #################################################
-
-# @app.route("/api/v1.0/justice-league")
-# def justice_league():
-#     """Return the justice league data as json"""
-
-#     return jsonify(justice_league_members)
-
-
-# @app.route("/")
-# def welcome():
-#     return (
-#         f"Welcome to the Justice League API!<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/justice-league<br/>"
-#         f"/api/v1.0/justice-league/superhero/batman"
-#     )
-
-
-# """TODO: Handle API route with variable path to allow getting info
-# for a specific character based on their 'superhero' name """
-
-# @app.route("/api/v1.0/justice-league/<superhero>")
-# def justice_league_character(superhero):
-#     """Fetch the Justice League character whose real_name matches
-#        the path variable supplied by the user, or a 404 if not."""
-
-#     canonicalized = superhero.replace(" ", "").lower()
-#     for character in justice_league_members:
-#         search_term = character["superhero"].replace(" ", "").lower()
-
-#         if search_term == canonicalized:
-#             return jsonify(character)
-
-#     return jsonify({"error": f"Character with real_name {superhero} not found."}), 404
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-# --------------------------------------------------
-#     
+ 
+import datetime as dt
 import numpy as np
+import pandas as pd
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -222,50 +43,129 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
+        f"Welcome to Hawaii, where the weather is great and the waves are perfect!<br/><br/>"
+        f"Is it raining in Hawaii? "
+        f"/api/v1.0/precipitation<br/><br/>"
+        f"Let's find the local weather stations on the island? "
+        f"/api/v1.0/stations<br/><br/>"
+        f"I want to go to the beach, what's the temperature like? "
+        f"/api/v1.0/tobs<br/><br/>"
+        f"My Hawaii vacation is almost here! What should I expect?<br/>"
         f"/api/v1.0/start<br/>"
         f"/api/v1.0/start/end<br/>"
     )
 
+#################################################
 
+# Precipitation api
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Return a list of all precipitation"""
-    # Query all precipitation
-    # results = session.query(Passenger.name).all()
+    # query the date and precipitation from the table "measurement" for the past year
+    querydt = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date.between('2016-08-23', '2017-08-23'))
 
-    # # Convert list of tuples into normal list
-    # all_names = list(np.ravel(results))
+    # Convert the query results to a Dictionary using date as the key and prcp as the value.
+    precipitation = []
+    for preci in querydt:
+        preci_dict = {}
+        preci_dict["Date"] = preci.date
+        preci_dict["Precipitation"] = preci.prcp
+        precipitation.append(preci_dict)
+        
+    # Return the JSON representation of your dictionary.
+    return jsonify(precipitation)
 
-    # return jsonify(all_names)
+#################################################
 
-    mdates = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+# Stations api
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all stations and their names"""
+    # query the stations table
+    querystation = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation)
 
-    query_date_yearago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Convert the query results to a Dictionary using station as the key and name as the value.
+    stations = []
+    for st in querystation:
+        st_dict = {}
+        st_dict["Station"] = st.station
+        st_dict["Name"] = st.name
+        st_dict["Latitude"] = st.latitude
+        st_dict["Longitude"] = st.longitude
+        st_dict["Elevation"] = st.elevation
+        stations.append(st_dict)
+        
+    # Return the JSON representation of your dictionary.
+    return jsonify(stations)
 
+#################################################
 
-    return jsonify(all_names)
+# Temperatures api
+@app.route("/api/v1.0/tobs")
+def temperatures():
+    """Return a list of all temperatures"""
+    # query the date and temperatures from the table "measurement" for the past year
+    querytobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date.between('2016-08-23', '2017-08-23'))
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger).all()
+    # query for the dates and temperature observations from a year from the last data point.
+    temperatures = []
+    for temp in querytobs:
+        temp_dict = {}
+        temp_dict["Date"] = temp.date
+        temp_dict["Temperature"] = temp.tobs
+        temperatures.append(temp_dict)
+        
+    # Return a JSON list of Temperature Observations (tobs) for the previous year.
+    return jsonify(temperatures)
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for passenger in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = passenger.name
-#         passenger_dict["age"] = passenger.age
-#         passenger_dict["sex"] = passenger.sex
-#         all_passengers.append(passenger_dict)
+#################################################
 
-#     return jsonify(all_passengers)
+# Average Temperatures api with start date only
+@app.route("/api/v1.0/<start>")
+def start(start):
+    """Return a list of all tmin, tavg, tmax temperatures with a start date only"""
 
+    trip_avg_temp = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= '2016-02-28')
 
+    # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+    avgtemp = []
+    for tmin, tavg, tmax in trip_avg_temp:
+        av_dict = {}
+        av_dict["Lowest Temperature"] = tmin
+        av_dict["Average Temperature"] = tavg
+        av_dict["Highest Temperature"] = tmax
+        avgtemp.append(av_dict)
+
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start range.
+    return jsonify(avgtemp)
+
+#################################################
+
+# Average Temperatures api with a start and end dates
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start, end):
+    """Return a list of all tmin, tavg, tmax temperatures with a start date only"""
+
+    start = '2016-02-28'
+    end = '2016-03-05'
+
+    trip_temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).\
+    filter(Measurement.date <= end)
+
+    # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    trip_start_end = []
+    for tmin, tavg, tmax in trip_temps:
+        tse_dict = {}
+        tse_dict["Lowest Temperature"] = tmin
+        tse_dict["Average Temperature"] = tavg
+        tse_dict["Highest Temperature"] = tmax
+        trip_start_end.append(tse_dict)
+
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start-end range.
+    return jsonify(trip_start_end)
+
+#################################################
 if __name__ == '__main__':
     app.run(debug=True)
